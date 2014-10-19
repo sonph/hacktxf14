b9c2d8ae4883ad03bc552c0810ef089cfb171ab8
Son Pham <sonph@compumatrice.com>
2014-10-19 02:41:26 -0500

    standalone version of webapp2 without GAE

--- web_handlers.py
+++ web_handlers.py
@@ -5,9 +5,7 @@ import os
 
 import webapp2
 import jinja2
-
-from google.appengine.api import users
-from google.appengine.ext import ndb
+import blpapi
 
 JINJA_ENV = jinja2.Environment(
     loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -20,7 +18,15 @@ class HomeHandler(webapp2.RequestHandler):
         template_values = {}
         template = JINJA_ENV.get_template('templates/index.html')
         self.response.write(template.render(template_values))
+        opts = blpapi.SessionOptions()
 
 app = webapp2.WSGIApplication([
     ('/', HomeHandler),
 ], debug=True)
+
+def main():
+    from paste import httpserver
+    httpserver.serve(app, host='127.0.0.1', port='8080')
+
+if __name__ == '__main__':
+    main()
