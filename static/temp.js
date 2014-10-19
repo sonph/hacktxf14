30bf2550f8f0da8c1538392a3fda6674f3c7706d
quangta93 <quang.tran@utdallas.edu>
2014-10-19 04:44:59 -0500

    commit before merge

--- static/temp.js
+++
@@ -1,7 +0,0 @@
-
-function init() {
-	var len = document.getElementsByTagName('p')[0].innerHTML.length * 5;
-	len = len + 'px';
-	console.log(len);
-	$(document).getElementById("marquee").css('min-width', len);
-}
