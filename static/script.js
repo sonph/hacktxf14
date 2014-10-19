5620b0f2333d76b9d592ba28d1af6f5864804bd8
quangta93 <quang.tran@utdallas.edu>
2014-10-19 09:37:55 -0500

    final commit

--- static/script.js
+++ static/script.js
@@ -2,25 +2,37 @@
 var timer;
 
 function init() {
-	timer = setTimeout(update, 10000);
+	timer = setTimeout(update, 1000);
 }
 
 /* updates stock prices through get request */
 function update() {
-	$.get('192.1.168.108:8080/api', function(data) {
-		var response = JSON.parse(data);
-		var arr = [];
-		if (response.udpate === 'true') {
-			// update content
-			arr = response.data.split(',');
-		}
-		var list = $(document).getElementById('list');
+	var xmlHttp = null;
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", 'http://192.168.1.108:8080/api', false );
+    xmlHttp.send( null );
+    var response = JSON.parse(xmlHttp.responseText);
+    
+	var arr = [];
+	// console.log(response['update']);
+	if (response['update'] == true) {
+		// update content
+		arr = response['data'].split(',');
+		var list = document.getElementById('list');
 		var content = '';
+		// console.log(arr.length);
 		for (var iter = 0; iter < arr.length; iter ++) {
 			if ((content + arr[iter]).length > 140) {
-				list.innerHTML += '<li>' + content + '</li>';
+				list.innerHTML += '<li>%</li>'.replace('%', content);
+				// console.log('content=' + content);
 				content = arr[iter];
+			} else {
+				content += arr[iter];
 			}
 		}
-	});
+		$(function ticker() {
+			$('#example').vTicker();
+		});
+		setTimeout(update, 20000);
+	}
 }
